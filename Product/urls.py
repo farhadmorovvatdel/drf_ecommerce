d49d4fe5a685e8440f0cb0410210bff2e515a494
farhad <f.morovvatdel@gmail.com>
2024-05-01 17:17:26 +0330

    add views and urls

--- Product/urls.py
+++ Product/urls.py
@@ -1,4 +1,8 @@
 from django .urls import path
+from .views import ProductListAPIView,ProductDetailAPIView,CatgeoryListAPIView
+
 urlpatterns=[
-    
+    path('',ProductListAPIView.as_view()),
+    path('<int:pk>/',ProductDetailAPIView.as_view()),
+    path('category/',CatgeoryListAPIView.as_view())
 ]
--- Product/views.py
+++ Product/views.py
@@ -1,3 +1,30 @@
 from django.shortcuts import render
+from rest_framework.response import Response
+from rest_framework.request import Request
+from rest_framework import status
+from .serializer import ProductSerializer,CategorySerializer
+from rest_framework import generics
+from .models import Product,Category
+from rest_framework.permissions import IsAuthenticated
 
-# Create your views here.
+
+
+class ProductListAPIView(generics.ListAPIView):
+    serializer_class=ProductSerializer
+    queryset=Product.objects.all()
+    
+
+class ProductDetailAPIView(generics.GenericAPIView):
+    serializer_class=ProductSerializer
+    queryset=Product.objects.all() 
+    
+    
+
+class CatgeoryListAPIView(generics.ListAPIView):
+    serializer_class=CategorySerializer
+    queryset=Category.objects.prefetch_related('products').all()
+   
+  
+        
+        
+   
