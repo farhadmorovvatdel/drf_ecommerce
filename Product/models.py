ae32e6203343ee8ae343f4da9fe090840402fe15
farhad <f.morovvatdel@gmail.com>
2024-05-01 17:16:20 +0330

    create model and add admin

--- Product/models.py
+++ Product/models.py
@@ -1,12 +1,14 @@
 from django.db import models
 from django.contrib.auth import get_user_model
-
+from django.contrib.contenttypes.fields import GenericForeignKey, GenericRelation
+from django.contrib.contenttypes.models import ContentType
 User=get_user_model()
+
+
+
 class Category(models.Model):
     name = models.CharField(max_length=100)
     description = models.TextField(blank=True)
-    
-
     def __str__(self):
         return self.name
 
@@ -18,13 +20,10 @@ class Product(models.Model):
     category = models.ForeignKey(Category, on_delete=models.CASCADE, related_name='products')
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
-   
-
     def __str__(self):
         return self.name
   
        
-
 class ProductImage(models.Model):
     product = models.ForeignKey(Product, on_delete=models.CASCADE, related_name='images')
     image = models.ImageField(upload_to='product_images/')
@@ -32,4 +31,35 @@ class ProductImage(models.Model):
    
     def __str__(self):
         return f"Image of {self.product.name}"
- 
+    
+class Comment(models.Model):
+    author=models.ForeignKey(User,on_delete=models.CASCADE,related_name='author')
+    product=models.ForeignKey(Product,on_delete=models.CASCADE,related_name='product')
+    text=models.TextField(blank=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    def __str__(self):
+        return f'{self.author} - {self.product}'
+    
+class Like(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+    content_type = models.ForeignKey(ContentType, on_delete=models.CASCADE)
+    object_id = models.PositiveIntegerField()
+    content_object = GenericForeignKey('content_type', 'object_id')
+
+    def __str__(self):
+         return f'{self.content_object} - {self.object_id}'
+
+    def unlike(self):
+        return  self.delete()
+
+class Dislike(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+    content_type = models.ForeignKey(ContentType, on_delete=models.CASCADE)
+    object_id = models.PositiveIntegerField()
+    content_object = GenericForeignKey('content_type','object_id')
+    def __str__(self):
+        return f'{self.content_object}- {self.object_id}'
+    def RemoveThislike(self):
+        return  self.delete()
