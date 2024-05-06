3d5fcb0e64820b83d77e53deb31dfbefb177ad69
farhad <f.morovvatdel@gmail.com>
2024-05-06 16:24:48 +0330

    add serializer for order and orderitem

--- Product/serializer.py
+++ Product/serializer.py
@@ -1,5 +1,5 @@
 from rest_framework import serializers
-from .models import Product,Category
+from .models import Product,Category,Order,OrderItem
 
 class ProductSerializer(serializers.ModelSerializer):
     class Meta:
@@ -11,3 +11,16 @@ class CategorySerializer(serializers.ModelSerializer):
     class Meta:
         model=Category
         fields=['name']
+
+
+class OrderItemSerializer(serializers.ModelSerializer):
+    # product = ProductSerializer() 
+    class Meta:
+        model=OrderItem
+        fields="__all__"
+ 
+ 
+class OrderSerializer(serializers.ModelSerializer):
+    class Meta:
+        model=Order
+        fields="__all__"
